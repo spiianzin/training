d57d3540501c03480c37ebb691433255b288ff45
Sergei Piianzin <Sergei_Piianzin@epam.com>
2019-12-11 17:34:28 +0300

    feat(folder2_ex_2): uncomment test; pass 100%

--- algorithms_book/folder_2/ex_2/queue/queue.go
+++ algorithms_book/folder_2/ex_2/queue/queue.go
@@ -1,34 +1,36 @@
 package queue
 
 type Queue struct {
-  head int
-  tail int
-  lenght int
-  capacity int
-  array [5]int
+	head     int
+	tail     int
+	lenght   int
+	capacity int
+	array    [5]int
 }
 
 func (q *Queue) Enqueue(val int) {
+	q.array[q.tail] = val
 
-  if ((q.tail + 1 == q.head) || (q.head == 0 && q.tail + 1 >=q.capacity)) {
-    return
-  }
+	q.tail++
 
-  q.array[q.tail] = val
-  q.tail++
-  if (q.tail >= q.capacity) {
-    q.tail = 0
-  }
+	if q.tail >= q.capacity {
+		q.tail = 0
+	}
 
-  // if (q.head + 1 <= q.capacity) {
-  //     q.array[q.head] = val
-  //     q.lenght++
-  //     q.head++
-  // }
+	// if (q.head + 1 <= q.capacity) {
+	//     q.array[q.head] = val
+	//     q.lenght++
+	//     q.head++
+	// }
 }
 
 func (q *Queue) Dequeue() int {
-  ret := q.array[q.head]
-  q.head++
-  return ret
+	ret := q.array[q.head]
+	q.head++
+
+	if q.head >= q.capacity {
+		q.head = 0
+	}
+
+	return ret
 }
--- algorithms_book/folder_2/ex_2/queue/queue_test.go
+++ algorithms_book/folder_2/ex_2/queue/queue_test.go
@@ -1,75 +1,104 @@
 package queue
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestEnqueue(t *testing.T) {
-  q := Queue {0, 0, 0, 5, [5]int{}}
-  input := 22
-  expected := 22
+	q := Queue{0, 0, 0, 5, [5]int{}}
+	input := 22
+	expected := 22
 
-  q.Enqueue(input)
-  output := q.array[0]
+	q.Enqueue(input)
+	output := q.array[0]
 
-  if output != expected {
-    t.Error("\nTest Failed: {} inserted, {} expected, recieved: {}", input, expected, output)
-  }
+	if output != expected {
+		t.Error("\nTest Failed: {} inserted, {} expected, recieved: {}", input, expected, output)
+	}
 }
 
 func TestDenqueue(t *testing.T) {
-  q := Queue {0, 0, 0, 5, [5]int{}}
-  expected := 22
+	q := Queue{0, 0, 0, 5, [5]int{}}
+	expected := 22
 
-  q.Enqueue(22)
-  q.Enqueue(30)
-  q.Enqueue(-5)
-  output := q.Dequeue()
+	q.Enqueue(22)
+	q.Enqueue(30)
+	q.Enqueue(-5)
+	output := q.Dequeue()
 
-  if output != expected {
-    t.Error("\nTest Failed: {} expected, recieved: {}", expected, output)
-  }
+	if output != expected {
+		t.Error("\nTest Failed: {} expected, recieved: {}", expected, output)
+	}
 }
 
 func TestShouldIncreaseTailIndexWhenAddElement(t *testing.T) {
-  q := Queue {0, 0, 0, 5, [5]int{}}
-  expected := 1
+	q := Queue{0, 0, 0, 5, [5]int{}}
+	expected := 1
 
-  q.Enqueue(22)
-  output := q.tail
+	q.Enqueue(22)
+	output := q.tail
 
-  if output != expected {
-    t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
-  }
+	if output != expected {
+		t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
+	}
+}
+
+func TestShouldSetToZeroTailIndexWhenAddElement(t *testing.T) {
+	q := Queue{0, 0, 0, 5, [5]int{}}
+	expected := 0
+
+	q.Enqueue(22)
+	q.Enqueue(23)
+	q.Enqueue(24)
+	q.Enqueue(25)
+	q.Dequeue()
+	q.Enqueue(26)
+	output := q.tail
+
+	if output != expected {
+		t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
+	}
+}
+
+func TestShouldIncreaseHeadIndexWhenPopElement(t *testing.T) {
+	q := Queue{0, 0, 0, 5, [5]int{}}
+	expected := 1
+
+	q.Dequeue()
+	output := q.head
+
+	if output != expected {
+		t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
+	}
+}
+
+func TestShouldSetToCapasityNumberHeadIndexWhenPopElement(t *testing.T) {
+	q := Queue{0, 0, 0, 5, [5]int{}}
+	expected := 4
+
+	q.Dequeue()
+	q.Dequeue()
+	q.Dequeue()
+	q.Dequeue()
+	output := q.head
+
+	if output != expected {
+		t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
+	}
 }
 
 func TestShouldRevertTailIndexWhenAddElement(t *testing.T) {
-  q := Queue {0, 0, 0, 5, [5]int{}}
-  expected := 0
+	q := Queue{0, 0, 0, 5, [5]int{}}
+	expected := 0
 
-  q.Enqueue(22)
-  q.Enqueue(23)
-  q.Enqueue(24)
-  q.Enqueue(25)
-  q.Dequeue()
-  q.Enqueue(26)
-  output := q.tail
+	q.Enqueue(22)
+	q.Enqueue(23)
+	q.Enqueue(24)
+	q.Enqueue(25)
+	q.Enqueue(26)
+	output := q.tail
 
-  if output != expected {
-    t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
-  }
+	if output != expected {
+		t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
+	}
 }
-
-// func TestShouldRevertTailIndexWhenAddElement2(t *testing.T) {
-//   q := Queue {0, 0, 0, 3, [3]int{}}
-//   expected := 0
-//
-//   q.Enqueue(22)
-//   q.Enqueue(23)
-//   q.Enqueue(24)
-//   output := q.tail
-//
-//   if output != expected {
-//     t.Errorf("\nTest Failed: %d expected, recieved: %d", expected, output)
-//   }
-// }
